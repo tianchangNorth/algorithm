bf2c8a658b102bfbb132155d8d300cafaa3b8f75
tianchangNorth <116717451+tianchangNorth@users.noreply.github.com>
2025-05-19 14:47:04 +0800

    feat: 添加所有子集和组合的算法及示例代码

---
+++ problem/22.所有子集.ts
@@ -0,0 +1,38 @@
+// 题目描述：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+// 说明：解集不能包含重复的子集。
+
+// 示例: 输入: nums = [1,2,3]
+// 输出:
+// [
+// [3],
+// [1],
+// [2],
+// [1,2,3],
+// [1,3],
+// [2,3],
+// [1,2],
+// []
+// ]
+
+// root                                            []
+// 数字1——第一层                     1                                   []
+// 数字2——第二层            [1,2]            [1]                   [2]         []
+// 数字3———第三层    [1,2,3]     [1,2]  [1,3]   [1]           [2,3]   [2]   [3]  []
+
+const subsets = (nums: any[]): [] => {
+  const res: any = []
+  const len = nums.length
+  const curr: any = []
+  const dfs = (index: number) => {
+    res.push([...curr])
+    for (let i = index; i < len; i++) {
+      curr.push(nums[i])
+      dfs(i + 1)
+      curr.pop()
+    }
+  }
+  dfs(0)
+  return res
+}
+
+console.log(subsets([1, 2, 3]));
---
+++ problem/23.n中可能的k个数的组合.ts
@@ -0,0 +1,31 @@
+// 题目描述：给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+// 示例: 输入: n = 4, k = 2
+// 输出:
+// [
+// [2,4],
+// [3,4],
+// [2,3],
+// [1,2],
+// [1,3],
+// [1,4],
+// ]
+
+const combine = (n: number, k: number): [] => {
+  const res: any = []
+  const curr: any = []
+  const dfs = (index: number) => {
+    if (curr.length === k) {
+      res.push([...curr])
+    }
+
+    for (let i = index; i <= n; i++) {
+      curr.push(i)
+      dfs(i + 1)
+      // curr.pop(i)
+    }
+  }
+  dfs(1)
+  return res
+}
+
+console.log(combine(4, 2));
