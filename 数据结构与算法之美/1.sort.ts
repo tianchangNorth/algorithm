83064dc15b51a0ddfd00f34aee47e106c7bde648
tianchangNorth <116717451+tianchangNorth@users.noreply.github.com>
2025-08-04 15:26:54 +0800

    feat: 添加排序算法实现，包括归并排序和快速排序

--- 数据结构与算法之美/1.sort.ts
+++ 数据结构与算法之美/1.sort.ts
@@ -44,6 +44,8 @@ function insertionSort(arr: number[]): number[] {
 
 
 // 选择排序
+// 空间复杂度为 O(1) 是一个原地排序算法
+// 选择排序是一种不稳定的排序算法，选择排序每次都要找剩余未排序元素中的最小值，并和前面的元素交换位置，这样破坏了稳定性。
 const changeSort = (nums: number[]): number[] => {
   for (let i = 0; i < nums.length; i++) {
     let j = i
@@ -61,3 +63,47 @@ const changeSort = (nums: number[]): number[] => {
 
 console.log(changeSort([5, 4, 3, 2, 1]));
 
+
+//从代码实现上来看，冒泡排序的数据交换要比插入排序的数据移动要复杂，冒泡排序需要 3 个赋值操作，而插入排序只需要 1 个，我们把执行一个赋值语句的时间粗略地计为单位时间（unit_time），
+// 然后分别用冒泡排序和插入排序对同一个逆序度是 K 的数组进行排序。用冒泡排序，需要 K 次交换操作，每次需要 3 个赋值语句，所以交换操作总耗时就是 3*K 单位时间。而插入排序中数据移动操作只需要 K 个单位时间
+
+
+// 归并排序
+// 归并排序是一种不原地但稳定的排序算法
+const mergeSort = (nums: number[]): number[] => {
+  if (nums.length <= 1) return nums
+
+  const mid = Math.floor(nums.length / 2)
+  const left = mergeSort(nums.slice(0, mid))
+  const right = mergeSort(nums.slice(mid))
+
+  return merge(left, right)
+}
+
+const merge = (left: number[], right: number[]): number[] => {
+  const res: number[] = []
+  let i = 0, j = 0
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      res.push(left[i])
+      i++
+    } else {
+      res.push(right[j])
+      j++
+    }
+  }
+
+  return res.concat(left.slice(i)).concat(right.slice(j));
+}
+
+// 快速排序
+// 快排是一种原地、不稳定的排序算法
+const quickSort = (nums: number[]): number[] => {
+  if (nums.length <= 1) return nums
+  const key = nums[0]
+  const right = nums.filter(item => item > nums[0])
+  const left = nums.filter(item => item < nums[0])
+  return [...quickSort(left), key, ...quickSort(right)]
+}
+
+console.log('quick', quickSort([5, 4, 3, 2, 1]));
